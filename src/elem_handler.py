ffb758d28cc07fd97fb9c49d7086010f6a04a019
jlgm <jlgm@cin.ufpe.br>
2018-05-25 13:11:16 -0300

    initial scraper

---
+++ src/elem_handler.py
@@ -0,0 +1,40 @@
+"""this module will deal with the parts of github urls"""
+
+class Elem(object):
+    """Class that provides utility for github urls"""
+
+    def __init__(self, url):
+        self.url = url
+
+    def is_blob(self):
+        """returns true if this elem is a file and false if it is a folder"""
+        parts = self.url.split('/')
+        if len(parts) < 4:
+            return False
+        return parts[3] == "blob"
+
+    def name(self):
+        """returns the name of the file or folder"""
+        parts = self.url.split('/')
+        return parts[-1]
+
+    def blob_to_raw(self):
+        """replaces 'blob' with 'raw' in this elem url, allowing the scraper to fetch only the file instead of the html"""
+        parts = self.url.split('/')
+        raw = parts[0]
+        for part in parts[1:]:
+            if part != "blob":
+                raw += ("/"+part)
+            else:
+                raw += "/raw"
+        return raw
+
+    def extension(self):
+        """returns the extension of this file if it has one"""
+        name = self.name()
+        if name[0] == '.':
+            return "<other>"
+        elif '.' not in name:
+            return "<other>"
+        else:
+            return name.split('.')[-1]
---
+++ src/scraper.py
@@ -0,0 +1,43 @@
+"""this module will deal with the scraping logic"""
+
+import requests
+
+from bs4 import BeautifulSoup
+from anytree import Node, RenderTree
+from elem_handler import Elem
+
+REQ = requests.Session()
+
+def dfs(current, parent=None, root="https://github.com"):
+    """this function does a DFS on the root of the repository and collects all the information we will need"""
+
+    elem = Elem(current)
+
+    if elem.is_blob():
+        page = REQ.get(root + elem.blob_to_raw())
+        lines = page.content.count('\n') + 1
+        cur_node = Node(elem.name(),
+                        parent=parent,
+                        lines=lines,
+                        size=len(page.content),
+                        extension=elem.extension())
+        return cur_node
+
+    cur_node = Node(elem.name()) if parent is None else Node(elem.name(), parent=parent)
+
+    page = REQ.get(root + current)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    for content in soup.find_all("td", {"class":"content"}):
+        for url in content.find_all("a"):
+            dfs(url["href"], cur_node)
+
+    return cur_node
+
+# TREE = dfs("/jlgm/QuakeParser")
+# for pre, fill, node in RenderTree(TREE):
+#     if hasattr(node, "lines"):
+#         print("%s%s (%d lines)" % pre, node.name, node.lines)
+#     else:
+#         print("%s%s" % pre, node.name)
+    
